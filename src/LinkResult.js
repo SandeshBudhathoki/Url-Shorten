7a0e8b07a38c10bf79cc86f758dc1b3ab571011e
Shadow <159432266+SandeshBudhathoki@users.noreply.github.com>
2024-05-30 20:08:26 +0545

    Add files via upload

---
+++ src/LinkResult.js
@@ -0,0 +1,69 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
+
+const LinkResult = ({ inputValue }) => {
+  const [shortenLink, setShortenLink] = useState("");
+  const [copied, setCopied] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchData = async () => {
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: "JWT fefege...",
+    };
+    try {
+      setLoading(true);
+      axios.post(`https://api-ssl.bitly.com/v4/shorten`, data, {
+        headers: headers
+      })
+      const res = await axios(
+        `https://api-ssl.bitly.com/v4/shorten?url=${inputValue}`
+      );
+      setShortenLink(res.data.result.full_short_link);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (inputValue.length) {
+      fetchData();
+    }
+  }, [inputValue]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [copied]);
+
+  if (loading) {
+    return <p className="noData">Loading...</p>;
+  }
+  if (error) {
+    return <p className="noData">Something went wrong</p>;
+  }
+
+  return (
+    <>
+      {shortenLink && (
+        <div className="result">
+          <p>{shortenLink}</p>
+          <CopyToClipboard text={shortenLink} onCopy={() => setCopied(true)}>
+            <button className={copied ? "copied" : ""}>
+              Copy to Clipboard
+            </button>
+          </CopyToClipboard>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default LinkResult;
